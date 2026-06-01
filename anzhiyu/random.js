3c04961ab3f2429fb30d26f3f5ef0a7d22276fe2
wj-wyt <wj-wyt@users.noreply.github.com>
2026-06-01 08:57:12 +0800

    Site updated: 2026-06-01 08:57:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/06/01/hello-world/"];function toRandomPost(){
+var posts=["2026/06/01/hello-world/","2026/06/01/xx文档批量高清导出PDF使用方法/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
